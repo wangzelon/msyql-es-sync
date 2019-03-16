f364663f76ec57a8936d0810b6cfad007717d13f
wwt <944799421@qq.com>
2019-03-16 16:15:51 +0800

    集成mybatis-plus

---
+++ src/main/java/com/opshop/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.opshop.config;
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * created by wangzelong 2019/3/16 16:12
+ */
+@Configuration
+@MapperScan("com.opshop.dao*")
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+}
