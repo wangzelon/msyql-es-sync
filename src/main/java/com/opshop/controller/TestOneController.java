aa834557babc6d94b020971357c652b05b8afa94
wwt <944799421@qq.com>
2019-03-21 11:47:12 +0800

    #删除无用代码

--- src/main/java/com/opshop/controller/TestOneController.java
+++ src/main/java/com/opshop/controller/TestOneController.java
@@ -1,7 +1,6 @@
 package com.opshop.controller;
 
 import com.opshop.core.SyncMysql;
-import com.opshop.entity.TestOne;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +14,6 @@ import java.io.IOException;
 public class TestOneController {
     @Autowired
     private SyncMysql syncMysql;
-
-    @GetMapping("/test")
-    public TestOne test() {
-        return null;
-    }
 
     @GetMapping("/add")
     public String testAdd() throws IOException {
